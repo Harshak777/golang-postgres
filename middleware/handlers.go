0531ee5816b0e2f5f68a67b9a8214f4776e650bd
Harshak777 <harshakkrishnaa777@gmail.com>
2024-04-14 18:39:29 -0700

    Added database queries

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -19,7 +19,7 @@ type Response struct {
 	Message string `json:"message,omitempty"`
 }
 
-func CreateConnection() *sql.DB {
+func createConnection() *sql.DB {
 	err := godotenv.Load(".env")
 
 	if err != nil {
@@ -136,4 +136,122 @@ func DeleteStock(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(res)
+}
+
+func insertStock(stock models.Stock) int64 {
+	db := createConnection()
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO stocks(name, price, comapny) VALUES(%1, $2, $3) RETURNING stockid`
+
+	var id int64
+	err := db.QueryRow(sqlStatement, stock.Name, stock.Price, stock.Company).Scan(&id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query %v", err)
+	}
+
+	fmt.Printf("Inserted a single record %v", id)
+
+	return id
+}
+
+func getStock(id int64) (models.Stock, error) {
+	db := createConnection()
+	defer db.Close()
+
+	var stock models.Stock
+
+	sqlStatement := `SELECT * FROM stocks WHERE stockid=$1`
+
+	row := db.QueryRow(sqlStatement, id)
+
+	err := row.Scan(&stock.StockID, &stock.Name, &stock.Company)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return stock, nil
+	case nil:
+		return stock, nil
+	default:
+		log.Fatalf("Unable to scan the row. %v", err)
+	}
+
+	return stock, err
+}
+
+func getAllStock() ([]models.Stock, error) {
+	db := createConnection()
+	defer db.Close()
+
+	var stocks []models.Stock
+
+	sqlStatement := `SELECT * FROM stocks`
+
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query %v", err)
+	}
+
+	for rows.Next() {
+		var stock models.Stock
+
+		err := rows.Scan(&stock.StockID, &stock.Name, &stock.Company)
+
+		if err != nil {
+			log.Fatalf("Unable to scan the row. %v", err)
+		}
+
+		stocks = append(stocks, stock)
+	}
+
+	return stocks, err
+}
+
+func updateStock(id int64, stock models.Stock) int64 {
+	db := createConnection()
+	defer db.Close()
+
+	sqlStatement := `UPDATE stocks SET name=$2, price=$3, company=$4 WHERE stockid=$1`
+
+	res, err := db.Exec(sqlStatement, id, stock.Name, stock.Price, stock.Company)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query %v", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/records affected %v", rowsAffected)
+
+	return rowsAffected
+}
+
+func deleteStock(id int64) int64 {
+	db := createConnection()
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM stocks WHERE stockid=$1`
+
+	res, err := db.Exec(sqlStatement, id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query %v", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/records affected %v", rowsAffected)
+
+	return rowsAffected
 }
